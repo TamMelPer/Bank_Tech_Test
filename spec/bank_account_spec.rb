ddcfca01c6d90cabd6d1d44bf7c23954ba3ec60d
Melissa <mystic101@gmail.com>
2021-01-11 15:38:45 +0000

    Refactor tests

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -4,18 +4,17 @@ require 'bank_account'
 
 describe BankAccount do
 
+subject(:account) { BankAccount.new }
+
   it 'opens a new bank account with a 0 balance' do
-    account = BankAccount.new
     expect(account.balance).to eq 0
   end
 
   it 'prints a blank statement' do
-    account = BankAccount.new
     expect{account.print_statement}.to output("date || credit || debit || balance").to_stdout
   end
 
   it 'updates the balance from 0 to 1000.00 when a deposit of 1000 is made' do
-    account = BankAccount.new
     account.deposit(1000)
     expect(account.balance).to eq 1000.00
   end

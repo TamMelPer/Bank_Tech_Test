7e96922e00d052a47961fe8ac162dae66f05a45c
Melissa <mystic101@gmail.com>
2021-01-11 12:42:00 +0000

    Opens a new bank account with a 0 balance

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 class BankAccount
+
+attr_reader :balance
+
+def initialize
+  @balance = 0
+end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 require 'bank_account'
 
 describe BankAccount do
-  
+
+  it 'opens a new bank account with a 0 balance' do
+    account = BankAccount.new
+    expect(account.balance).to eq 0
+  end
+
 end

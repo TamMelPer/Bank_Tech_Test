68fd8cc3e2d176644f0f9c382536b0801073acd2
Melissa <mystic101@gmail.com>
2021-01-13 20:45:41 +0000

    update BankAccount class to simplify code by storing the transactions as a list of Transaction instances directly.

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -38,25 +38,25 @@ describe BankAccount do
   describe '#print_statement' do
     it 'prints a statement with the date, credit and balance after a deposit is made' do
       account.deposit(1000)
-      expect { account.print_statement }.to output("date || credit || debit || balance\n11/01/2021 || 1000.00 ||  || 1000.00\n").to_stdout
+      expect { account.print_statement }.to output("date || credit || debit || balance\n11/01/2021 || 1000.00 || 0.00 || 1000.00\n").to_stdout
     end
 
     it 'prints a statement with the date, credit and balance after a different deposit amount is made' do
       account.deposit(2000)
-      expect { account.print_statement }.to output("date || credit || debit || balance\n11/01/2021 || 2000.00 ||  || 2000.00\n").to_stdout
+      expect { account.print_statement }.to output("date || credit || debit || balance\n11/01/2021 || 2000.00 || 0.00 || 2000.00\n").to_stdout
     end
 
     it 'prints a statement with the date, credit and balance after two deposits are made in reverse chronological order' do
       account.deposit(1000)
       account.deposit(2000)
-      expect { account.print_statement }.to output("date || credit || debit || balance\n11/01/2021 || 2000.00 ||  || 3000.00\n11/01/2021 || 1000.00 ||  || 1000.00\n").to_stdout
+      expect { account.print_statement }.to output("date || credit || debit || balance\n11/01/2021 || 2000.00 || 0.00 || 3000.00\n11/01/2021 || 1000.00 || 0.00 || 1000.00\n").to_stdout
     end
 
     it 'prints a statement with the date, credit and balance after a deposit and withdrawal is made in reverse chronological order' do
       account.deposit(1000)
       account.deposit(2000)
       account.withdraw(500)
-      expect { account.print_statement }.to output("date || credit || debit || balance\n11/01/2021 ||  || 500.00 || 2500.00\n11/01/2021 || 2000.00 ||  || 3000.00\n11/01/2021 || 1000.00 ||  || 1000.00\n").to_stdout
+      expect { account.print_statement }.to output("date || credit || debit || balance\n11/01/2021 || 0.00 || 500.00 || 2500.00\n11/01/2021 || 2000.00 || 0.00 || 3000.00\n11/01/2021 || 1000.00 || 0.00 || 1000.00\n").to_stdout
     end
   end
 end
